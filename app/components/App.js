6f792f98c3b593d299f1afa7090e166f0fc15e94
Tenzin Tsagong <tsagong@gmail.com>
2017-07-01 16:17:54 -0400

    lang-selection

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,12 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
-
+var Popular = require('./Popular')
 
 class App extends React.Component {
   render() {
     return(
-      <div>Hello World!</div>
+      <div>
+        <Popular />
+      </div>
     )
   }
 }
